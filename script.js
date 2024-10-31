1dc96214810e0679f257a2ae52574dbeb2c1502d
Ahmet Aknur <ahmetovaaknur520@gmail.com>
2024-10-31 13:43:27 +0600

    hm 30.10

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+var modelName = "matiz";
+var color = "green";
+var wheelsCount = "4";
+var isOffRoad = "no";
+var rating = "4.7";
 
+console.log("matyx", modelName);
+console.log("green", color);
+console.log("4", wheelsCount);
+console.log("no", isOffRoad);
+console.log("4,7", rating);
