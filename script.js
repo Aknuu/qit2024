a2c4825cfed095422c2ca3aff51c0b24d85b2a65
Ahmet Aknur <ahmetovaaknur520@gmail.com>
2024-10-31 17:59:51 +0600

    tap

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-var modelName = "matiz";
-var color = "green";
-var wheelsCount = "4";
-var isOffRoad = "no";
-var rating = "4.7";
+// var modelName = "matiz";
+// var color = "green";
+// var wheelsCount = "4";
+// var isOffRoad = "no";
+// var rating = "4.7";
 
-console.log("matyx", modelName);
-console.log("green", color);
-console.log("4", wheelsCount);
-console.log("no", isOffRoad);
-console.log("4,7", rating);
+// console.log("matyx", modelName);
+// console.log("green", color);
+// console.log("4", wheelsCount);
+// console.log("no", isOffRoad);
+// console.log("4,7", rating);
+
+// var a = 16;
+// var b = 17
+// console.log(a-b)
+
+// var a = 45; //
+// var b = 25;
+// var obshii = a * b;
+// console.log(obshii);
+
+// var num1 = 20; //
+// let num2 = 15;
+// console.log(num1 / num2);
+
+// var aknur = 130;
+// aknur++;
+// aknur++; // плюс ставим перед ровно
+// aknur++;
+// aknur += 20;  // aknur = aknur + 20
+// aknur -=30;
+// console.log(aknur);
+
+// var a =200;
+// a %= 8;
+// console.log(a);
+
+var a = 50;
+var b = 15;
+var c = 7;
+var d = 55;
+var e = 8;
+
+console.log(a + b) * c - d / e;
