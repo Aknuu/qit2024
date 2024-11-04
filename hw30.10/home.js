32eba62717834f242a79e80d6de86762d04783fa
Ahmet Aknur <ahmetovaaknur520@gmail.com>
2024-11-04 17:27:15 +0600

    commit

---
+++ hw30.10/home.js
@@ -0,0 +1,25 @@
+// var modelName = "matiz";
+// var color = "green";
+// var wheelsCount = "4";
+// var isOffRoad = "no";
+// var rating = "4.7";
+
+// console.log("matyx", modelName);
+// console.log("green", color);
+// console.log("4", wheelsCount);
+// console.log("no", isOffRoad);
+// console.log("4,7", rating);
+
+let slicesPerPizza = 8;
+let numberOfPizzas = 5;
+let totalSlices = slicesPerPizza * numberOfPizzas;
+console.log("Барлық тілімдер саны: " + totalSlices);
+
+let totalChocolates = 24;
+let numberOfFriends = 6;
+let chocolatesPerFriend = totalChocolates / numberOfFriends;
+console.log("Әр дос қанша шоколад алады: " + chocolatesPerFriend);
+
+let plantHeight = 10;
+plantHeight += 2;
+console.log("Бір аптадан кейін өсімдіктің биіктігі: " + plantHeight + " см");
