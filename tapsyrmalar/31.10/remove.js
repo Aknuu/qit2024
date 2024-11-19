cbafbb3522308eeeec3bb7c47716c01159df305a
Ahmet Aknur <ahmetovaaknur520@gmail.com>
2024-11-19 18:56:26 +0600

    jhjhhj

--- tapsyrmalar/31.10/remove.js
+++ tapsyrmalar/31.10/remove.js
@@ -1,23 +1,18 @@
-let num1 = +prompt("birinshi san"); //5
-let num2 = +prompt("ikinci san");  //3
-let tanba = prompt("tanba"); //+ - * /
+let money = +prompt("Бірінші ақшаны енгіз");
 
-function calculator(a, b, t) {
-  if (t == "+") {
-    return a + b;
-  }else if (t == "-"){
-    return a - b;
-  }else if(t=="*"){
-    return a * b;
-  }else if(t=="/"){
+let currency = prompt(
+  "Валюта енгіз:" + "\n dollar to tenge немесе tenge  to dollar (tg/$)"
+);
 
+const moneyConvert = (x, z) => {
+  switch (z) {
+    case "tg":
+      return x / 496.92;  //1500
+      break;
+    case "$":
+      return x * 496.92;
+      break;
   }
-}
+};
 
-var jauap = calculator(num1, num2, tanba);
-
-alert(jauap);
-
-// const constructor = (x,y,z) => {}
-
-// alert(result = constructor(num1, num2, tanda) )
+alert("Конвертация нәтижесі:" + moneyConvert(money, currency));
