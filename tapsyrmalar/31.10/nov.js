e3a2e07a45b1d3e343d4abd257e6b5f13d57bd76
Ahmet Aknur <ahmetovaaknur520@gmail.com>
2024-11-25 15:40:36 +0600

    remove

---
+++ tapsyrmalar/31.10/nov.js
@@ -0,0 +1,30 @@
+// // var sum = 0;
+// // for (let i = 1; i <= 100; i++) {
+// //     sum +=i;
+// // }
+// // console.log("сума чисел от 1 до 100,sum");
+
+
+
+// // for  (let i = 1; i <= 100; i++) {
+// //     console.log(i);
+    
+// // }
+
+
+
+// var resultJup = 0;
+// var resultTaq = 0;
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 == 0) {
+//     resultJup += i;
+//   } else {
+//     resultTaq += i;
+//   }
+// }
+
+// console.log("Jup sandar qosyndysy:${resultJup}");
+// console.log("Taq sandar qosyndysy:${resultTaq}");
+
+
--- tapsyrmalar/31.10/remove.js
+++ tapsyrmalar/31.10/remove.js
@@ -1,18 +1,37 @@
-let money = +prompt("Бірінші ақшаны енгіз");
+// 2 тапсырма сандарды косу
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+  sum += i; 
+}
+console.log("Сумма чисел от 1 до 100:", sum);
 
-let currency = prompt(
-  "Валюта енгіз:" + "\n dollar to tenge немесе tenge  to dollar (tg/$)"
-);
 
-const moneyConvert = (x, z) => {
-  switch (z) {
-    case "tg":
-      return x / 496.92;  //1500
+// 1 так немесе жуп
+let number = prompt("Введите число:"); 
+if (number % 2 === 0) {
+  console.log("Число четное.");
+} else {
+  console.log("Число нечетное.");
+}
+
+
+// карапайм сан табу
+let num = prompt("Введите число:");
+let isPrime = true;
+
+if (number <= 1) {
+  isPrime = false; 
+} else {
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      isPrime = false;
       break;
-    case "$":
-      return x * 496.92;
-      break;
+    }
   }
-};
+}
 
-alert("Конвертация нәтижесі:" + moneyConvert(money, currency));
+if (isPrime) {
+  console.log("Число является простым.");
+} else {
+  console.log("Число не является простым.");
+}
